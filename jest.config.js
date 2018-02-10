0a4b148f905f9423353fb37bec6a9a2251a42318
Denis Beketsky <tatsu.ukraine@gmail.com>
2018-02-10 13:59:58 +0200

    init

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'vue'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  mapCoverage: true
+}
